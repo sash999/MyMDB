10221cc84d65a6bb751d90ac42a4794fc4de1d1b
sash999 <sash999@yandex.ru>
2018-11-02 12:49:09 +0300

    page 42 finished

--- django/config/settings.py
+++ django/config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm-j&)0t5$@22*k_n0vf9k%53@aiopud!=bp*zk2loaerkdk7p-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'loclhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
