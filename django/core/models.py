9cde8813b122088e247fc715132112a66f29d43c
sash999 <sash999@yandex.ru>
2018-11-07 13:00:00 +0300

    page 49 finished, migrations done

--- django/core/models.py
+++ django/core/models.py
@@ -22,6 +22,22 @@ class Movie(models.Model):
         default = NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
+    director = models.ForeignKey(
+        to = 'Person',
+        on_delete = models.SET_NULL,
+        related_name = 'directed',
+        null = True,
+        blank = True)
+    writers = models.ManyToManyField(
+        to = 'Person',
+        related_name = 'writing_credits',
+        blank = True)
+    actors = models.ManyToManyField(
+        to = 'Person',
+        through = 'Role',
+        related_name = 'acting_credits',
+        blank = True)
+
 
     class Meta:
         ordering = ('title', '-year',)
@@ -29,4 +45,39 @@ class Movie(models.Model):
     def __str__(self):
         return '{} ({})'.format(
             self.title, self.year)
-    
+
+class Person(models.Model):
+    first_name = models.CharField(max_length = 140)
+    last_name = models.CharField(max_length = 140)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('last_name', 'first_name')
+
+    def __str__(self):
+        if slef.died:
+            return '{}, {} ({}-{})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died)
+        return '{}, {} ({})'.format(
+            self.last_name,
+            self.first_name,
+            self.born)
+
+
+
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length = 140)
+    def __str__(self):
+        return '{} {} {}'.format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        unique_together = ('movie', 'person', 'name')
+
